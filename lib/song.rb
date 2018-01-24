7cf23fadc99d74b614aee6cc8a3ea3728150fdee
Jennifer Dobak <jdobak1@gmail.com>
2018-01-24 15:13:39 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+
+  attr_reader :name, :artist, :genre
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @@artists << artist
+  @genre = genre
+  @@genres << genre
+  @@count += 1
+end
+
+@@count = 0
+@@genres = []
+@@artists = []
+
+def count
+  return @@count
+end
+
+def artists
+end
+
+def artist_count
+#how many songs each artist created
+end
+
+def genres
+
+end
+
+def genre_count
+  #how many songs in each genre
+  genre_count = {}
+end
+
+
+end
