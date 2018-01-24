be777f0e857c742012f1e20bd77d976793a88d88
Jennifer Dobak <jdobak1@gmail.com>
2018-01-24 15:54:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ end
 def self.genre_count
   #how many songs in each genre
   genre_count = {}
-  @@genre.uniq.each do |genre|
+  @@genres.uniq.each do |genre|
   genre_count[genre] = genre.size
 end
 genre_count
